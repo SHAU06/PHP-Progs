6a088ea16b02890d0f74c2b55d5196666a53c6ec
Gaurav Borse <46493526+Mrlifewarior@users.noreply.github.com>
2021-10-12 14:54:38 +0530

    Added Program to Reverse the String
    A string can be reversed either using strrev() function or simple PHP code.
    For example, on reversing JAVATPOINT it will become TNIOPTAVAJ.

---
+++ reverse_string.php
@@ -0,0 +1,9 @@
+// Reverse String
+// A string can be reversed either using strrev() function or simple PHP code.
+
+// For example, on reversing JAVATPOINT it will become TNIOPTAVAJ.
+
+<?php  
+$string = "JAVATPOINT";  
+echo "Reverse string of $string is " .strrev ( $string );  
+?>
